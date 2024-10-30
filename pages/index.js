3f220b020ec125b20cc6f123e885965bc343400e
Jocy Gonçalves Neto <jocygneto@gmail.com>
2024-10-30 14:59:31 +0000

    adicionando arquivos iniciais: manisfetos, versionamento do node e pagina inicial sem implementacao

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <div>
+      <h1>Dudu você me inspira se você gostou disso da um risadinha! 😎</h1>
+    </div>
+  );
+}
+
+export default index;
